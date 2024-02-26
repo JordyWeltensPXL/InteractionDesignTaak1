d4911bc4e8b830120d6f551b4b8beb9c4fcb8f5b
JordyWeltensPXL <92799790+JordyWeltensPXL@users.noreply.github.com>
2024-02-26 22:30:29 +0100

    added heading, cover and song info

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
 export default {
   data() {
     return {
-      info: ["Playing from your library", "Liked Songs"],
+      info: [
+        "Playing from your library",
+        "Liked Songs",
+        "Plastic Beach",
+        "Gorillaz",
+      ],
     };
   },
 };
@@ -10,9 +15,18 @@ export default {
 
 <template>
   <div class="mediaplayer">
-    <div class="info">
-      <h2 class="info__title">{{ info[0] }}</h2>
-      <p class="info__subtitle">{{ info[1] }}</p>
+    <div class="heading">
+      <i class="fa-solid fa-chevron-down"></i>
+      <div class="info">
+        <h2 class="info__title">{{ info[0] }}</h2>
+        <p class="info__subtitle">{{ info[1] }}</p>
+      </div>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    </div>
+    <img class="cover" src="./assets/094.gif" alt="album gif" />
+    <div class="song">
+      <h2 class="song__title">{{ info[2] }}</h2>
+      <p class="song__description">{{ info[3] }}</p>
     </div>
   </div>
 </template>
