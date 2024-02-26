885a2a9e7953cb02d2b8d2254b12025c61941bbd
JordyWeltensPXL <92799790+JordyWeltensPXL@users.noreply.github.com>
2024-02-26 18:44:12 +0100

    cleanup + adding variables

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      info: ["Playing from your library", "Liked Songs"],
+    };
+  },
+};
 </script>
 
 <template>
-  <div>
-    <div class="mbappe"></div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="mediaplayer">
+    <h2 class="info__title">{{ info[0] }}</h2>
+    <p class="info__subtitle">{{ info[1] }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
