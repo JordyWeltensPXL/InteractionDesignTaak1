80c0859ad76ce45b9e8a4e8388a67b4a24d44a14
JordyWeltensPXL <92799790+JordyWeltensPXL@users.noreply.github.com>
2024-02-26 23:02:49 +0100

    media player close to done

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,20 @@ export default {
       <h2 class="song__title">{{ info[2] }}</h2>
       <p class="song__description">{{ info[3] }}</p>
     </div>
+    <div class="progress">
+      <progress class="progress__bar"></progress>
+      <div class="progress__time">
+        <p class="progress__time-start">1:25</p>
+        <p class="progress__time-end">3:34</p>
+      </div>
+    </div>
+    <div class="controls">
+      <i class="fa-regular fa-heart fa-sm"></i>
+      <i class="fa-solid fa-backward-step fa-lg"></i>
+      <i class="fa-solid fa-circle-pause fa-2xl"></i>
+      <i class="fa-solid fa-forward-step fa-lg"></i>
+      <i class="fa-solid fa-ban fa-sm"></i>
+    </div>
   </div>
 </template>
 
